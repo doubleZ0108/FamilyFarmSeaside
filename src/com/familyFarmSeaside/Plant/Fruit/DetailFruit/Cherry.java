074e525854b34b6e624d29a05f107a4c331c8bf7
double Z <doubleZ0108@163.com>
2019-10-26 18:29:33 +0800

    merge DocGroup

---
+++ src/com/familyFarmSeaside/Plant/Fruit/DetailFruit/Cherry.java
@@ -0,0 +1,13 @@
+package com.familyFarmSeaside.Plant.Fruit.DetailFruit;
+
+import com.familyFarmSeaside.Plant.Fruit.Fruit;
+
+/**
+ * @program: FamilyFarmSeaside
+ * @description:
+ * @author: doubleZ
+ * @create: 2019/10/25
+ **/
+public class Cherry extends Fruit {
+
+}
