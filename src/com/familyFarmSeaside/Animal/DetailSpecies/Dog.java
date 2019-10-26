3e17969f8758cc031970e85b191a40e6e682d651
LXF-DX3906 <1621413639@qq.com>
2019-10-26 14:41:50 +0800

    add prototype pattern of animal clone

--- src/com/familyFarmSeaside/Animal/DetailSpecies/Dog.java
+++ src/com/familyFarmSeaside/Animal/DetailSpecies/Dog.java
@@ -1,6 +1,7 @@
 package com.familyFarmSeaside.Animal.DetailSpecies;
 
 import com.familyFarmSeaside.Animal.Animal;
+import com.familyFarmSeaside.Animal.Clone;
 import com.familyFarmSeaside.Animal.Species.Mammals;
 
 /**
@@ -9,9 +10,20 @@ import com.familyFarmSeaside.Animal.Species.Mammals;
  * @author: doubleZ
  * @create: 2019/10/24
  **/
-public class Dog extends Mammals {
+public class Dog extends Mammals implements Clone {
     public Dog() {
         super();
         detailedSpecies = "DOG";
+    }
+
+    public Dog(Dog dog) {
+        species = dog.species;
+        detailedSpecies = dog.detailedSpecies;
+        sex = dog.sex;
+        age = dog.age;
+    }
+
+    public Dog clone(){
+        return new Dog(this);
     }
 }
--- src/com/familyFarmSeaside/Animal/DetailSpecies/Perch.java
+++ src/com/familyFarmSeaside/Animal/DetailSpecies/Perch.java
@@ -1,5 +1,6 @@
 package com.familyFarmSeaside.Animal.DetailSpecies;
 
+import com.familyFarmSeaside.Animal.Clone;
 import com.familyFarmSeaside.Animal.Species.Fish;
 
 /**
@@ -8,9 +9,20 @@ import com.familyFarmSeaside.Animal.Species.Fish;
  * @author: doubleZ
  * @create: 2019/10/24
  **/
-public class Perch extends Fish {
+public class Perch extends Fish implements Clone {
     public Perch() {
         super();
         detailedSpecies = "PERCH";
+    }
+
+    public Perch(Perch perch) {
+        species = perch.species;
+        detailedSpecies = perch.detailedSpecies;
+        sex = perch.sex;
+        age = perch.age;
+    }
+
+    public Perch clone(){
+        return new Perch(this);
     }
 }
