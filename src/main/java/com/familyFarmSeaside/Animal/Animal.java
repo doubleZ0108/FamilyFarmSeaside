497f9a6e94009d89bcf4a9325c4fe0c39dfeef6b
LXF-DX3906 <1621413639@qq.com>
2019-10-27 17:17:21 +0800

    add prototype pattern of animal clone 完善了获取动物属性的方法 完善了动物的breed方法 增加了PrototypeTest测试类

--- src/main/java/com/familyFarmSeaside/Animal/Animal.java
+++ src/main/java/com/familyFarmSeaside/Animal/Animal.java
@@ -2,6 +2,8 @@ package main.java.com.familyFarmSeaside.Animal;
 
 import main.java.com.familyFarmSeaside.Product.Product;
 
+import java.util.Random;
+
 /**
  * @program: FamilyFarmSeaside
  * @description: The base class of all Animal.
@@ -9,28 +11,41 @@ import main.java.com.familyFarmSeaside.Product.Product;
  * @create: 2019/10/24
  **/
 public abstract class Animal extends Product {
-    static protected enum SpeciesName {MAMMALS,POULTRY,FISH};  //哺乳类 禽类 鱼类
-    protected SpeciesName species;          //种类
-    protected String detailedSpecies;  //具体类别
+    static protected enum SpeciesName {MAMMALS,POULTRY,FISH}  //哺乳类 禽类 鱼类
+    static protected enum Sex {MALE, FEMALE}                                   //雄性 雌性
+    static protected enum LifeStage {BABY, ADULT}                            //幼年体 成熟体
 
-    static protected enum Sex {MALE, FEMAIL};
-    protected Sex sex;
-
-    static protected enum LifeStage {BABY, ADULT};  //幼年体 成熟体
-    protected LifeStage lifestage;      //生命阶段
-
-    protected int age;      //年龄
-    protected int hungerValue;  //饥饿值(0~100) 0 -> 非常饿       【undo redo pattern】
+    protected SpeciesName species;                                                    //种类
+    protected String detailedSpecies;                                                   //具体类别
+    protected Sex sex;                                                                         //性别
+    protected LifeStage lifestage;                                                        //生命阶段
+    protected int age;      								                                   //年龄
+    protected int hungerValue;  							                               //饥饿值(0~100) 0 -> 非常饿       【undo redo pattern】
 
     public Animal() {
         this.lifestage = LifeStage.BABY;
         this.age = 1;
+        this.hungerValue=100;
+        Random rand = new Random();
+        if(rand.nextInt(2) + 0==1) {
+            this.sex = Sex.MALE;
+        }
+        else {
+            this.sex=Sex.FEMALE;
+        }
     }
+
+    abstract  public SpeciesName getSpecies();
+    abstract  public String getDetailedSpecies();
+    abstract  public  Sex getSex();
+    abstract  public LifeStage getLifestage();
+    abstract  public  int getAge();
+    abstract  public  int getHungerValue();
 
     abstract public void eat();
     abstract public void sleep();
     abstract public void mating();  //交配
-    abstract public void breed();   //繁殖
+    abstract public Animal breed();   //繁殖
     public void grow(){
         this.lifestage = LifeStage.ADULT;
     }
--- src/main/java/com/familyFarmSeaside/Animal/DetailSpecies/Dog.java
+++ src/main/java/com/familyFarmSeaside/Animal/DetailSpecies/Dog.java
@@ -2,6 +2,8 @@ package main.java.com.familyFarmSeaside.Animal.DetailSpecies;
 
 import main.java.com.familyFarmSeaside.Animal.Species.Mammals;
 
+import java.util.Random;
+
 /**
  * @program: FamilyFarmSeaside
  * @description:
@@ -12,6 +14,36 @@ public class Dog extends Mammals {
     public Dog() {
         super();
         detailedSpecies = "DOG";
+    }
+
+    @Override
+    public SpeciesName getSpecies() {
+        return this.species;
+    }
+
+    @Override
+    public String getDetailedSpecies() {
+        return this.detailedSpecies;
+    }
+
+    @Override
+    public Sex getSex() {
+        return this.sex;
+    }
+
+    @Override
+    public LifeStage getLifestage() {
+        return this.lifestage;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public int getHungerValue() {
+        return this.hungerValue;
     }
 
   @Override
@@ -30,12 +62,44 @@ public class Dog extends Mammals {
   }
 
   @Override
-  public void breed() {
-
+  public Dog breed() {
+        return  new Dog();
   }
 
   @Override
   protected void run() {
 
   }
+
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Dog newdog = (Dog) super.clone();
+        Random rand = new Random();
+        if(rand.nextInt(2) + 0==1) {
+            newdog.sex = Sex.MALE;
+        }
+        else {
+            newdog.sex=Sex.FEMALE;
+        }
+        return newdog;
+    }
+
+    @Override
+    public  boolean equals(Object obj){
+        if (this == obj)      //传入的对象是其本身，相等的；
+            return true;
+        if (obj == null)     //如果传入的对象为空，不相等
+            return false;
+        if (getClass() != obj.getClass())  //如果不是同一个类型的，不相等
+            return false;
+        Dog dog = (Dog) obj;
+        if(this.detailedSpecies==dog.detailedSpecies && this.species.equals(dog.species)
+                && this.sex==dog.sex&&this.age==dog.age&&this.lifestage==dog.lifestage){
+            return true;
+        }
+        return false;
+    }
+
 }
