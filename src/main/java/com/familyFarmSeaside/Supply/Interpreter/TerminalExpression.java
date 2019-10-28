d2decc9ce5983ce309c01f785a7213051a3c9ec1
leaf222 <ahtlye@163.com>
2019-10-28 15:27:30 +0800

    a small change

---
+++ src/main/java/com/familyFarmSeaside/Supply/Interpreter/TerminalExpression.java
@@ -0,0 +1,12 @@
+package main.java.com.familyFarmSeaside.Supply.Interpreter;
+
+/**
+ * @program: FamilyFarmSeaside
+ * @description:Terminal expression, implements the interface Expression.
+ * @author: Yifan Ye
+ * @create: 2019/10/28
+ **/
+public class TerminalExpression
+{
+    
+}
