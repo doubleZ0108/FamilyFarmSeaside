7368bdc8d046da39073d77b4aec7943034972912
leaf222 <ahtlye@163.com>
2019-11-02 14:41:18 +0800

    Robuster!

--- src/test/AdapterTest.java
+++ src/test/AdapterTest.java
@@ -4,6 +4,8 @@ import main.java.com.familyFarmSeaside.Animal.DetailSpecies.Dog;
 import main.java.com.familyFarmSeaside.Watchdog.Adapter;
 import main.java.com.familyFarmSeaside.Watchdog.Watchdog;
 
+import java.util.Scanner;
+
 /**
  * @program: FamilyFarmSeaside
  * @description: The test class for Adapter Pattern
@@ -11,10 +13,13 @@ import main.java.com.familyFarmSeaside.Watchdog.Watchdog;
  * @create: 2019/10/28
  **/
 public class AdapterTest {
-    static  public  void testAdapter(String name) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----适配器模式测试----");
         System.out.println("创建一个对象watchdog...");
         Watchdog watchdog= new Adapter(new Dog());
         System.out.println("给watchdog取一个名字...");
+        String name = scanner.nextLine();
         watchdog.setName(name);
         System.out.println("watchdog的名字是："+watchdog.getName());
         System.out.println(watchdog.getName()+"吃...");
@@ -23,9 +28,5 @@ public class AdapterTest {
         watchdog.sleep();
         System.out.println(watchdog.getName()+"吠...");
         watchdog.bark();
-    }
-
-    public static void main(String[] args) {
-        testAdapter("popo");
     }
 }
